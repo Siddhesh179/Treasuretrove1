811f9c526fc3090c81db8f60d1f27b5e17f17d7e
Siddhesh179 <siddhesh8@outlook.com>
2024-07-27 12:23:44 +0530

    commiting

---
+++ treasure/src/App.jsx
@@ -0,0 +1,16 @@
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import Header from "./Header.jsx";
+import Home from "./Home.jsx";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Home />
+    </>
+  );
+}
+
+export default App;
